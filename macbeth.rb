2af6c30b5d0411c3a0f57430bde5272973725c7e
jayachakladar <jayachak6@gmail.com>
2014-07-25 15:46:56 -0700

    second commit

---
+++ macbeth.rb
@@ -0,0 +1,31 @@
+class Macbeth
+
+  attr_accessor :doc, :name_array, :sorted_list
+
+  def initialize
+    read_xml
+    find_actors
+    count_lines
+  end
+
+  def print_lines
+    @sorted_list.map{ |x| puts "#{x[1]} #{x[0]}"}
+  end
+
+  private
+  def count_lines
+    b = Hash.new(0)
+    @name_array.each{|v| b[v] += 1}
+    @sorted_list = b.sort_by{|k,v| v}.reverse!
+  end
+
+
+  def read_xml
+    @doc = Nokogiri::XML(open("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"))
+  end
+
+  def find_actors
+    @name_array = []
+    @doc.xpath("//SPEAKER").children.each{|x| @name_array << x.content()}
+  end
+end
--- macbeth_analyzer.rb
+++ macbeth_analyzer.rb
@@ -1,12 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
+require './macbeth.rb'
 
-
-doc = Nokogiri::XML(open("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"))
-name_array = []
-doc.xpath("//SPEAKER").children.each{|x| name_array << x.content()}
-b = Hash.new(0)
-name_array.each{|v|  b[v] += 1}
-sorted_list = b.sort_by{|k,v| v}.reverse!
-sorted_list.map{ |x| puts "#{x[1]} #{x[0]}"}
+aa = Macbeth.new
+aa.print_lines
 
